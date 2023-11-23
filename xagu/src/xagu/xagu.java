96926446e9d4a385d9e039cbc80006e0cc68d13d
DAM1 <DAM1@DESKTOP-HHSQF2N>
2023-11-23 09:31:19 +0100

    update 2311

--- xagu/src/xagu/xagu.java
+++ xagu/src/xagu/xagu.java
@@ -1,8 +1,6 @@
 package xagu;
-
 import java.awt.EventQueue;
 import java.awt.FileDialog;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -10,16 +8,17 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
-
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.border.BevelBorder;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+
 
 public class xagu extends JFrame {
 
@@ -31,9 +30,9 @@ public class xagu extends JFrame {
 	private JButton btnMapa;
 	private JPanel pLab;
 	private JLabel[][] gridLab = new JLabel[10][10];
-    private Font f=new Font("Agency FB", Font.BOLD, 18);
-    private FileDialog dlgMapa =  new FileDialog(xagu.this,"Cargar Mapa", FileDialog.LOAD);
-
+	private Font f=new Font("Agency FB", Font.BOLD, 18);
+	private FileDialog dlgMapa =  new FileDialog(xagu.this,"Cargar Mapa", FileDialog.LOAD);
+	private File mapa;
 	/**
 	 * Launch the application.
 	 */
@@ -50,9 +49,9 @@ public class xagu extends JFrame {
 		});
 	}
 
-//CONSTRUCTOR
+	//CONSTRUCTOR
 	public xagu() {
-		
+
 		// JFRAME ---> VENTANA
 		setTitle("XAGU");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -62,24 +61,24 @@ public class xagu extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
-		
+
 		// HEADER
 		pHeader = new JPanel();
 		contentPane.add(pHeader, BorderLayout.NORTH);
 		pHeader.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
+
 		btnMapa = new JButton("Cargar Mapa");
 		pHeader.add(btnMapa);
 		btnMapa.setFont(f);
-		
+
 		btnStart = new JButton("Start");
 		btnStart.setFont(f);
 		pHeader.add(btnStart);
-		
+
 		btnExit = new JButton("Exit");
 		pHeader.add(btnExit);
 		btnExit.setFont(f);
-		
+
 		// GRID LABELS
 		pLab = new JPanel();
 		pLab.setBorder(null);
@@ -87,47 +86,48 @@ public class xagu extends JFrame {
 		pLab.setLayout(new GridLayout(10, 10, 0, 0));
 		pLab.setBorder(BorderFactory.createLineBorder(Color.black));	
 		rellenarGrid();	
-		
+
 		// 
-		
-		
+
+
 		regEvents();
 	}
-//REGEVENTS
+	//REGEVENTS
 	private void regEvents()
 	{
 		btnMapa.addActionListener(new ActionListener() {
-			
-		
+
+
 			public void actionPerformed(ActionEvent e) {
 				dlgMapa.setDirectory(".//map");
 				dlgMapa.setVisible(true);
 				//obtener el fichero elegido. controlar cierre X
-				dlgMapa.getFile();
-				
+				darValores();
+
+
 			}
 		});
-		
+
 		btnExit.addActionListener(new ActionListener() {
-			
-			
+
+
 			public void actionPerformed(ActionEvent e) {
 
-				
+
 			}
 		});
-		
+
 		btnStart.addActionListener(new ActionListener() {
-			
+
 
 			public void actionPerformed(ActionEvent e) {
 
-				
+
 			}
 		});
 	}
 
-//FUNCTIONS
+	//FUNCTIONS
 	public void rellenarGrid()
 	{
 		for (int i=0; i<gridLab.length; i++)
@@ -142,6 +142,42 @@ public class xagu extends JFrame {
 			}
 		}
 	}
-	
+	public void darValores()
+	{
+		if(dlgMapa.getFile()!=null)
+		{
+			try 
+			{
+				Scanner scan = new Scanner(new File(dlgMapa.getDirectory()+dlgMapa.getFile()));
+				String linea;
+				int fila=0;
+				while(scan.hasNext())
+				{
+					linea=scan.next();
+					for (int i = 0; i < 10; i++) 
+					{
+						//gridLab[fila][i].setText(null);
+						//gridLab[fila][i].setIcon(null);
+						/*if(linea.charAt(i)=='1')
+						{
+							gridLab[fila][i].setIcon(new ImageIcon("img1/muro.jpg"));
 
+						}
+						if(linea.charAt(i)=='2')
+						{	
+							gridLab[fila][i].setIcon(new ImageIcon("img1/xagu.jpg"));
+						}*/
+					}
+					fila++;
+
+				}
+			} catch (FileNotFoundException e1) {
+
+			}
+		}
+
+	}
 }
+
+
+
